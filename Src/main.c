8f7e5f8917c613d305924bb2c2447ac1e6ec32ce
Maximiliano Mancilla <mmancilla.torres@iteso.mx>
2025-01-02 02:47:01 -0600

    Adding input functionaly for PB7. If PB7 is TRUE turn on LED PB3

--- Src/main.c
+++ Src/main.c
@@ -4,6 +4,7 @@
  * User LD3: the green LED is a user LED connected to Arduino Nano signal D13
  * corresponding to the STM32 I/O PB3 (pin 26).
  * | AHB2 | 0x4800 0400 - 0x4800 07FF | 1 KB | GPIOB
+ * PIN D4(5) PB7 AS INPUT ABH2 ALSO
  * ==========================================================================
  * */
 #include <stdint.h>
@@ -11,6 +12,7 @@
 
 #define PIN3                (1U<<3) 
 #define USER_LED_PIN        (PIN3)
+#define PIN7                (1U<<7)
 #define GPIOAEN             (1U<<0)
 #define GPIOBEN             (1U<<1)
 
@@ -19,25 +21,29 @@ int main(void)
     /* Enable clock access to GPIOB */
     RCC->AHB2ENR |= GPIOBEN;
 
-    /* Set PB3 AS OUTPUT */
+    /* Set PB3 AS OUTPUT MODE B7 = 0, B6 =  1*/
     GPIOB->MODER |= (1U<<6);
     GPIOB->MODER &=~ (1U<<7);
+
+    /* Set PB7 as input B14 = 0, B15 = 0 */
+    GPIOB->MODER &=~ (1U<<14);
+    GPIOB->MODER &=~ (1U<<15); 
 
     while(1)
     {
         //GPIOB->ODR |= USER_LED_PIN;  /* Turn on the LED */
         //GPIOB->ODR &= ~USER_LED_PIN; /* Turn off the LED */
         //GPIOB->ODR ^= USER_LED_PIN;  /* Toggle the LED */
-
-        /* Turn on the LED using bit set/reset register */
-        GPIOB->BSRR = USER_LED_PIN;
-        /* Delay */
-        for(int i = 0; i < 100000; i++){}
-        /* Turn off the LED using bit set/reset register */
-        GPIOB->BSRR = (1U << 19);
-        /* Delay */
-        for(int i = 0; i < 100000; i++){}
+        //for(int i = 0; i < 100000; i++){} /* Delay */
+        
+        if(GPIOB->IDR & PIN7) /* Check if the button is pressed */
+        {
+            GPIOB->BSRR = USER_LED_PIN;
+        }
+        else
+        {
+            GPIOB->BSRR = (1U << 19);
+        }
     }
-
     return 0;
 }
